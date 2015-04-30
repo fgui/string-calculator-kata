8c3b59ea8def32209907367a39f7adf6577b7811
francesc <francesc.guillen@gmail.com>
2015-04-30 15:22:52 +0200

    comment

--- src/string_calculator/core.clj
+++ src/string_calculator/core.clj
@@ -1,6 +1,11 @@
 (ns string-calculator.core
   (:use [clojure.string :as str :only [join]]))
 
+;; After finishing the kata I thought that there was a much
+;; easier way to do it, taking a shortcut:
+;; Parse numbers considering anything else delimiters.
+;; This code takes the shortcut.
+
 (defn str->int [str-num]
   (Integer/parseInt str-num))
 
