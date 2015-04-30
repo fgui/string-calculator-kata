9c6b7f49121f9c2694344d568139e4fb67d74ce7
francesc <francesc.guillen@gmail.com>
2015-04-30 15:13:55 +0200

    new version parse numbers any other string is a delimiter.

--- src/string_calculator/core.clj
+++ src/string_calculator/core.clj
@@ -1,33 +1,13 @@
 (ns string-calculator.core
-  (:use [clojure.string :as str :only [split join]]))
-
-(defn re-quote [s]
-  (java.util.regex.Pattern/quote s))
-
-(defn re-delimiters [delimeters]
-  (if (empty? delimeters)
-    #",|\n"
-    (re-pattern (str/join "|"  (cons ",|\\n" (map re-quote delimeters))))))
+  (:use [clojure.string :as str :only [join]]))
 
 (defn str->int [str-num]
-  (if (empty? str-num)
-    0
-    (Integer/parseInt str-num)))
-
-(defn extract-delimeters [delimeters]
-  (if-let [d (re-seq #"\[([^\]]+)\]" delimeters)]
-    (map second d)
-    [delimeters]))
-
-(defn str->delimeters-and-numbers-str [str-cmd]
-  (if-let [g (re-seq #"^//(.+)\n(.+)" str-cmd)]
-    (let [[dels nums]  (rest (first g))]
-      [(extract-delimeters dels) nums])
-    [[] str-cmd]))
+  (Integer/parseInt str-num))
 
 (defn extract-numbers [str-cmd]
-  (let [[delimeters str-nums] (str->delimeters-and-numbers-str str-cmd)]
-    (map str->int (str/split str-nums (re-delimiters delimeters)))))
+  (if-let [n (re-seq #"([-\d]+)" str-cmd)]
+    (map str->int (map second n))
+    [0]))
 
 (defn check-pos [numbers]
   (if (some neg? numbers)
